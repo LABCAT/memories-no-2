559bc6f36e19cbd659e7a8fb5bdfd2f4798fc41d
LABCAT <shane.watters.nz@gmail.com>
2022-08-07 19:10:31 +1200

    Implementing initial drawing functionality and integration of audio.

--- src/js/P5SketchWithAudio.js
+++ src/js/P5SketchWithAudio.js
@@ -5,8 +5,8 @@ import * as p5 from "p5";
 import { Midi } from '@tonejs/midi'
 import PlayIcon from './functions/PlayIcon.js';
 
-import audio from "../audio/circles-no-3.ogg";
-import midi from "../audio/circles-no-3.mid";
+import audio from "../audio/memories-no-2.ogg";
+import midi from "../audio/memories-no-2.mid";
 
 const P5SketchWithAudio = () => {
     const sketchRef = useRef();
@@ -25,11 +25,59 @@ const P5SketchWithAudio = () => {
 
         p.PPQ = 3840 * 4;
 
+        p.files = [
+            'Raohe-Street-Night-Market-2018.json',
+            'Cuba-St-Festival-2018.json',
+            'Tian-Tan-Buddha-2019.json',
+            'Lego-Land-Johor-Bahru-2015.json',
+        ];
+
+        p.imageData = {};
+
+        p.shapeData = [];
+
+        p.nextShapeData = [];
+
+        p.shapeTypes = ['circle', 'equilateral', 'rect', 'pentagon', 'hexagon', 'octagon'];
+
+        p.loadShapeData = () => {
+            // const startPos = multiplier <= 2 ? multiplier : multiplier + 2;
+            let array = [];
+            // const cicleSizes = [8,12,16];
+            const cicleSizes = [4, 8, 12];
+            let x = 2, y = 2;
+            while (x < p.width) {
+                let xSize = p.random(cicleSizes);
+                while (y < p.height) {
+                    const key = x + ',' + y,
+                        size = p.random(cicleSizes),
+                        shape = p.imageData[key];
+                    if(typeof shape !== typeof undefined) {
+                        array.push(
+                            {
+                                x: x,
+                                y: y,
+                                size: size,
+                                shapeType: p.random(p.shapeTypes),
+                                colour: shape.colour,
+                            }
+                        );
+                    }
+                    y = y + size;
+                }
+                x = x + xSize;
+                y = 2;
+            }
+
+            p.shapeData = p.shuffle(array);
+        }
+
         p.loadMidi = () => {
             Midi.fromUrl(midi).then(
                 function(result) {
-                    const noteSet1 = result.tracks[5].notes; // Synth 1
-                    p.scheduleCueSet(noteSet1, 'executeCueSet1');
+                    console.log(result);
+                    p.noteSet1 = result.tracks[2].notes; // Sampler 1 - RhodesMK1
+                    p.scheduleCueSet(p.noteSet1, 'executeCueSet1');
                     p.audioLoaded = true;
                     document.getElementById("loader").classList.add("loading--complete");
                     document.getElementById("play-icon").classList.remove("fade-out");
@@ -39,6 +87,7 @@ const P5SketchWithAudio = () => {
         }
 
         p.preload = () => {
+            p.imageData = require('../json/' + p.random(p.files));
             p.song = p.loadSound(audio, p.loadMidi);
             p.song.onended(p.logCredits);
         }
@@ -61,6 +110,11 @@ const P5SketchWithAudio = () => {
         p.setup = () => {
             p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
             p.background(0);
+            p.rectMode(p.CENTER);
+            p.noLoop();
+            p.noFill();
+            p.strokeWeight(0.5);
+            p.loadShapeData();
         }
 
         p.draw = () => {
@@ -69,14 +123,108 @@ const P5SketchWithAudio = () => {
             }
         }
 
+        p.currentNoteIndex = 1;
+
         p.executeCueSet1 = (note) => {
-            p.background(p.random(255), p.random(255), p.random(255));
-            p.fill(p.random(255), p.random(255), p.random(255));
-            p.noStroke();
-            p.ellipse(p.width / 2, p.height / 2, p.width / 4, p.width / 4);
+            if(p.currentNoteIndex === 12) {
+                p.clear();
+                p.background(0);
+                p.imageData = require('../json/' + p.random(p.files));
+                p.loadShapeData();
+                p.currentNoteIndex = 1;
+            }
+            const numItemsToDraw = Math.ceil(p.shapeData.length / 11); 
+            const startIndex = numItemsToDraw * p.currentNoteIndex - numItemsToDraw; 
+            const endIndex = p.shapeData.length < numItemsToDraw * p.currentNoteIndex ? p.shapeData.length : numItemsToDraw * p.currentNoteIndex;
+            for (let i = startIndex; i < endIndex; i++) {
+                const shape = p.shapeData[i],
+                    {x, y, size, shapeType, colour } = shape;
+
+                p.stroke(colour[0], colour[1], colour[2]);
+                p[shapeType](x, y, size, size);
+            }
+
+            p.currentNoteIndex++;
         }
 
         p.hasStarted = false;
+
+        /*
+        * function to draw an equilateral triangle with a set width
+        * based on x, y co-oridinates that are the center of the triangle
+        * @param {Number} x        - x-coordinate that is at the center of triangle
+        * @param {Number} y      	- y-coordinate that is at the center of triangle
+        * @param {Number} width    - radius of the hexagon
+        */
+        p.equilateral = (x, y, width) => {
+            var x1 = x - (width/2);
+            var y1 = y + (width/2);
+            var x2 = x;
+            var y2 = y - (width/2);
+            var x3 = x + (width/2);
+            var y3 = y + (width/2);
+            p.triangle(x1,y1,x2,y2,x3,y3);
+        }
+
+        /*
+        * function to draw a pentagon shape
+        * adapted from: https://p5js.org/examples/form-regular-polygon.html
+        * @param {Number} x        - x-coordinate of the pentagon
+        * @param {Number} y      - y-coordinate of the pentagon
+        * @param {Number} radius   - radius of the pentagon
+        */
+        p.pentagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            var angle = p.TWO_PI / 5;
+            p.beginShape();
+            for (var a = p.TWO_PI/10; a < p.TWO_PI + p.TWO_PI/10; a += angle) {
+                var sx = x + p.cos(a) * radius;
+                var sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
+        }
+
+        /*
+        * function to draw a hexagon shape
+        * adapted from: https://p5js.org/examples/form-regular-polygon.html
+        * @param {Number} x        - x-coordinate of the hexagon
+        * @param {Number} y      - y-coordinate of the hexagon
+        * @param {Number} radius   - radius of the hexagon
+        */
+        p.hexagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            var angle = p.TWO_PI / 6;
+            p.beginShape();
+            for (var a = p.TWO_PI/12; a < p.TWO_PI + p.TWO_PI/12; a += angle) {
+                var sx = x + p.cos(a) * radius;
+                var sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
+        }
+
+        /*
+        * function to draw a octagon shape
+        * adapted from: https://p5js.org/examples/form-regular-polygon.html
+        * @param {Number} x        - x-coordinate of the octagon
+        * @param {Number} y      - y-coordinate of the octagon
+        * @param {Number} radius   - radius of the octagon
+        */
+        p.octagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            var angle = p.TWO_PI / 8;
+            p.beginShape();
+            for (var a = p.TWO_PI/16; a < p.TWO_PI + p.TWO_PI/16; a += angle) {
+                var sx = x + p.cos(a) * radius;
+                var sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
+        }
 
         p.mousePressed = () => {
             if(p.audioLoaded){
